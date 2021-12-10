3f3fdcc33d5cc8cf68f839447742fa65fa4fbe1c
raihan-778 <raihanarif778@gmail.com>
2021-12-10 20:02:36 +0600

    first commit

---
+++ Js/main.js
@@ -0,0 +1,8 @@
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoicmFpaGFuNzc4IiwiYSI6ImNrd3A0aGJjZTA5OGIybm8yMm51dWs5MGsifQ.45rnc0Jlbd9AmJV7-5_H5Q';
+  var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.060982,42.35725],
+    zoom: 18  
+});
